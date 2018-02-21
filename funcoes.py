976387c3aafe0fae213d9a6bdcff2b52c55c2bf3
Your Name <you@example.com>
2018-02-21 15:22:31 -0300

    Versão 1.3

--- funcoes.py
+++ funcoes.py
@@ -7,23 +7,33 @@
 def calcula_inss(salario_bruto):
     if salario_bruto <= 1000.00:
         return 0.0
-    if salario <= 2000.00:
+    if salario_bruto <= 2000.00:
         return salario_bruto * 0.1
     else:
         return salario_bruto * 0.2
 
-#Essa função faz o cálculo de desconto por dependentes
-def dependentes(numero_dependentes):
-    desconto = 100.00
-    desconto_dependentes = desconto * numero_dependentes
-    return desconto_dependentes
+# Essa função faz o cálculo do desconto por número de dependentes
+def calc_dependentes(num_dependentes):
+    desc = 100.00
+    desc_dependentes = desc * num_dependentes
+    return desc_dependentes
 
-def imposto de renda(salario_liquido):
-    if salario_liquido <= 1400.00:
+# Essa função faz o cálculo do imposto de renda
+def imposto_de_renda(salario_bruto):
+    if salario_bruto <= 1400.00:
         return 0.0
-    if salario_liquido <= 2500.00:
-        return salario_liquido * 0.12
-    if salario_liquido <= 5000.00:
-        return salario_liquido * 0.2
+    if salario_bruto <= 2500.00:
+        return (salario_bruto * 0.12) - calcula_inss(sal_bruto) - calc_dependentes(num_dependentes)
+    if salario_bruto <= 5000.00:
+        return (salario_bruto * 0.2) - calcula_inss(sal_bruto) - calc_dependentes(num_dependetes)
     else:
-        return salario_liquido * 0.27
+        return (salario_bruto * 0.27) - calcula_inss(sal_bruto) - calc_dependentes(num_dependetes)
+
+sal_bruto = input('Digite o salário bruto: ')
+num_dependentes = input('Digite o número de dependentes: ')
+
+def calc_salario_liquido(sal_bruto):
+    salario_liquido = sal_bruto - calcula_inss(sal_bruto) - imposto_de_renda(sal_bruto)
+    return salario_liquido
+
+print(calc_salario_liquido(sal_bruto))
