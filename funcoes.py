cea5677729f34e006088af2c302c6d72128255d7
Your Name <you@example.com>
2018-02-21 14:53:08 -0300

    Versão 1.1

--- funcoes.py
+++ funcoes.py
@@ -3,6 +3,13 @@
 # Aula de Nivelamento em Python Básico
 # Exercício Funções em Python
 
+# Essa função faz o cálculo do INSS
 def calcula_inss(salario_bruto):
     inss = salario_bruto * 0.2
     return inss
+
+#Essa função faz o cálculo de desconto por dependentes
+def dependentes(numero_dependentes):
+    desconto = 100.00
+    desconto_dependentes = desconto * numero_dependentes
+    return desconto_dependentes
