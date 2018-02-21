f7f15d219b99e1c7ad76713216d46f1c06d74cca
Your Name <you@example.com>
2018-02-21 14:56:11 -0300

    Versão 1.1

--- funcoes.py
+++ funcoes.py
@@ -5,8 +5,12 @@
 
 # Essa função faz o cálculo do INSS
 def calcula_inss(salario_bruto):
-    inss = salario_bruto * 0.2
-    return inss
+    if salario_bruto <= 1000.00:
+        return 0.0
+    if salario <= 2000.00:
+        return salario_bruto * 0.1
+    else:
+        return salario_bruto * 0.2
 
 #Essa função faz o cálculo de desconto por dependentes
 def dependentes(numero_dependentes):
