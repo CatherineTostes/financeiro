8fb05d379fcab9322e4dc906d1df41ad28267770
Your Name <you@example.com>
2018-02-21 14:36:58 -0300

    Exercício Funções - primeiro função

---
+++ funcoes.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Catherine Tostes
+# Aula de Nivelamento em Python Básico
+# Exercício Funções em Python
+
+def calcula_inss(salario_bruto):
+    inss = salario_bruto * 0.2
+    return inss
